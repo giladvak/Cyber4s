5881c192af7f1c8734208e74831bf2cca9b78560
giladvak <102975677+giladvak@users.noreply.github.com>
2022-04-26 19:58:38 +0300

    order

--- Chess/chess.js
+++ Chess/chess.js
@@ -11,28 +11,17 @@ const KNIGHT = 'knight';
 const BISHOP = 'bishop';
 const KING = 'king';
 const QUEEN = 'queen';
+const container = [ROOK, KNIGHT, BISHOP, KING, QUEEN, BISHOP, KNIGHT, ROOK];
 
 
+function onClickFunc(row, col) {
 
+  boardData.clearClasses(row, col)
 
-
-
-boardData = new BoardData(getInitialBoard(),typeWhite);
-boardData.getPiece()
-
-
-
-
-function onClickFunc(e, shora, amoda, cell) {
-
- boardData.clearClasses(shora,amoda)
-  selectedCell = e.currentTarget;
-
+  selectedCell = table.rows[row].cells[col];
   const imgSelectedCell = selectedCell.firstElementChild
- 
+  const chosenPiece = boardData.getPiece(row, col)
 
-
-  const chosenPiece = boardData.getPiece(shora, amoda)
   if (chosenPiece) {
     selectedCell.classList.add('selected');
 
@@ -40,19 +29,18 @@ function onClickFunc(e, shora, amoda, cell) {
 
     for (let possibleMove of showPossibleMoves) {
       const cellPossibleMove = table.rows[possibleMove[0]].cells[possibleMove[1]];
-     
       const possiblePiece = boardData.getPiece(possibleMove[0], possibleMove[1]);
-      
+
       if (possiblePiece === undefined) {
 
         cellPossibleMove.classList.add('possibleMoves');
       }
-      if (possiblePiece !== undefined && possiblePiece.player !== boardData.getPiece(shora, amoda).player) {
+      if (possiblePiece !== undefined && possiblePiece.player !== boardData.getPiece(row, col).player) {
 
         cellPossibleMove.classList.add('enemy');
       }
-      
-      cellPossibleMove.addEventListener('click', (e) => { movePieces(e, cellPossibleMove, shora, amoda, table, imgSelectedCell, selectedCell,possibleMove)
+
+      cellPossibleMove.addEventListener('click', (e) => {movePieces(e,row, col, imgSelectedCell, possibleMove)
 
       });
 
@@ -67,43 +55,41 @@ function onClickFunc(e, shora, amoda, cell) {
 }
 
 
-function movePieces(e, cellPossibleMove, shora, amoda, table, imgSelectedCell,selectedCell,possibleMove) {
-  
- let chosenPiece= boardData.getPiece(shora, amoda)
+function movePieces(e, row, col, imgSelectedCell, possibleMove) {
 
-   selectedCell = e.currentTarget
+  let chosenPiece = boardData.getPiece(row, col)
+  let selectedCell = e.currentTarget
+  let updateRow = (selectedCell.id[0]) * 1
+  let updateCol = (selectedCell.id[2]) * 1
+  const icon = selectedCell.firstElementChild
   
-  let updateRow = (selectedCell.id[0])*1
-  let updateCol = (selectedCell.id[2])*1
-  if(chosenPiece===undefined) return;
+  if (chosenPiece === undefined) return;
   chosenPiece.row = updateRow;
   chosenPiece.col = updateCol;
-let secondPlayer=  boardData.getPiece(possibleMove[0],possibleMove[1])
+
+  let secondPlayer = boardData.getPiece(possibleMove[0], possibleMove[1])
+
   
-  const icon = selectedCell.firstElementChild
-  if(secondPlayer.player!==chosenPiece.player){
-   boardData.removePiece(possibleMove[0],possibleMove[1])
-   selectedCell.firstElementChild.remove()
-   selectedCell.appendChild(imgSelectedCell)
-   chosenPiece.row=possibleMove[0]
-   chosenPiece.col=possibleMove[1]
-   
-
-
-   
+  if (secondPlayer.player !== chosenPiece.player) {
+    boardData.removePiece(possibleMove[0], possibleMove[1])
+    
+    // chosenPiece.row = possibleMove[0]
+    // chosenPiece.col = possibleMove[1]
+    // selectedCell.appendChild(imgSelectedCell)
+    // selectedCell.firstElementChild.remove()
   }
 
   if (icon) return;
 
 
   selectedCell.appendChild(imgSelectedCell);
-  console.log(boardData.getPiece(shora,amoda))
+  if (boardData.currentPlayer === typeWhite) {
+    boardData.currentPlayer = typeBlack
+  } else {
+    boardData.currentPlayer = typeWhite
+  } console.log(selectedCell)
+  console.log(chosenPiece)
   console.log(boardData)
-  if(boardData.currentPlayer===typeWhite){
-    boardData.currentPlayer=typeBlack
-  }else{
-    boardData.currentPlayer=typeWhite
-  }
 }
 
 
@@ -116,14 +102,13 @@ let secondPlayer=  boardData.getPiece(possibleMove[0],possibleMove[1])
 function getInitialBoard() {
 
   let result = [];
-  let container = [ROOK, KNIGHT, BISHOP, KING, QUEEN, BISHOP, KNIGHT, ROOK];
   for (let i = 0; i < container.length; i++) {
     result.push(new Piece(7, i, container[i], typeBlack))
     result.push(new Piece(0, i, container[i], typeWhite))
     result.push(new Piece(1, i, PAWN, typeWhite))
     result.push(new Piece(6, i, PAWN, typeBlack))
   }
-  
+
   return result;
 }
 
@@ -137,11 +122,11 @@ function getImg(cell, type, name) {
 
 
 function createBoard() {
-  
+
   const div = document.createElement('div');
   document.body.appendChild(div);
   div.className = 'outerBox'
- 
+
   const divNum = document.createElement('div')
   const divLetters = document.createElement('div')
   div.appendChild(divNum);
@@ -160,59 +145,33 @@ function createBoard() {
     divLetters.appendChild(innerDivLetters);
     innerDivLetters.innerText = arr[i]
   }
-  //creating table 
   table = document.createElement('table');
   div.appendChild(table);
   table.classList.add('board');
-  //inserting tr
 
-  for (let shora = 0; shora < 8; shora++) {
-    const row = document.createElement('tr');
-    table.appendChild(row);
+  for (let row = 0; row < 8; row++) {
+    const rowElement = table.insertRow()
 
-    //inserting td's inside each tr
-    for (let amoda = 0; amoda < 8; amoda++) {
-      const cell = document.createElement('td');
-      row.appendChild(cell)
-      cell.id = (shora.toString() + ',' + amoda.toString())
-
-      //appending classes on condition 
-      if (amoda % 2 === shora % 2) {
-        cell.classList.add('white')
-
-
+    for (let col = 0; col < 8; col++) {
+      const cellElement = rowElement.insertCell()
+      cellElement.id = (row.toString() + ',' + col.toString())
+      if (col % 2 === row % 2) {
+        cellElement.classList.add('white')
       } else {
-        cell.classList.add('black');
+        cellElement.classList.add('black')
+      };
+      cellElement.addEventListener('click', () => onClickFunc(row, col))
+    };
+  } boardData = new BoardData(getInitialBoard(), typeWhite);
 
-      }
-
-
-      //calling event listener on each cell 
-      cell.addEventListener('click', (e) => onClickFunc(e, shora, amoda, cell));
-
-
-
-
-    }
-
-  };
-
-
-  //calling getImg for each hatha
-  for (let hatha of boardData.hatihot) {
-    getImg(table.rows[hatha.row].cells[hatha.col], hatha.player, hatha.type);
-
-
+  for (let piece of boardData.pieces) {
+    getImg(table.rows[piece.row].cells[piece.col], piece.player, piece.type);
   }
 
-
-
-
-};
+}
 
 
 window.addEventListener('load', createBoard)
-
 
 
 
